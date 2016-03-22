4b4bd1ed17914deb6215853dbffa6d3fa3a19fe6
juany360 <juaninv@outlook.com>
2016-03-22 13:14:14 -0300

    izi

--- Gauss/prueba.cpp
+++ Gauss/prueba.cpp
@@ -74,7 +74,7 @@ class Matriz{
 		}
 
 		void Permutar(int i, int j){
-			vector<int> aux = m[i];
+			vector<int> aux = m[j];
 			m[j] = m[i];
 			m[i] = aux;
 
@@ -106,7 +106,7 @@ class Matriz{
 				}			
 			}
 			cout << endl;
-		}
+		}		
 
 		Matriz Gauss(){
 
@@ -123,15 +123,32 @@ class Matriz{
 			return res;
 		};
 
-		Matriz Gauss0(vector<int> p){
+		void Reordenar(vector<int> &p, int i, int j){
+			int aux = p[i];
+			p[i] = p[j];
+			p[j] = aux;
+		}
+
+		Matriz Gauss0(vector<int> &p){
 
 			Matriz res = Matriz(filas);
 
 			for (int i = 0; i <columnas-1; ++i){						//COLUMNAS
 				for (int j = i+1; j < filas; ++j){				//FILAS
 					
-
-					
+					if(m[i][i] == 0){
+						int h = i;
+						while(h<columnas){
+							if(m[h][i] != 0){
+								Permutar(i,h);
+								res.Permutar(i,h);
+								Reordenar(p,i,h);
+								h = h +columnas;
+							}
+							h++;
+						}
+					}					
+										
 					int aux = m[j][i]/m[i][i];					
 					res.Cambiar(j,i,aux);
 					m[j][i] = m[j][i]-aux*m[i][i];			
@@ -143,17 +160,31 @@ class Matriz{
 		
 };
 
+void Imprimir(vector<int> p){
+			cout << "Imprimiendo vector de permutacion" << endl;
+			for (int i = 0; i < p.size(); ++i){
+				if(i == 0){cout << "[";}				
+				cout << p[i];
+				if(i == p.size()-1){cout << "]";}
+				else{cout << ",";}
+			}
+			cout << endl;
+			cout << endl;
+}
+
 int main() {
 
+	
+
 	vector< int > a;
-	a.push_back(2);
+	a.push_back(1);
 	a.push_back(2);
 	a.push_back(3);
 	a.push_back(3);
 	a.push_back(4);
 	vector< int > b;
 	b.push_back(1);
-	b.push_back(2);
+	b.push_back(0);
 	b.push_back(3);
 	b.push_back(3);
 	b.push_back(4);
@@ -183,20 +214,39 @@ int main() {
 	m.push_back(d);
 	m.push_back(f);
 
+	vector<int> p;
+	p.push_back(1);
+	p.push_back(2);
+	p.push_back(3);
+	p.push_back(4);
+	p.push_back(5);
+	p.push_back(6);
+
 	Matriz h(m);
 	
-	/* Prueba gauss sin 0
+	/* Prueba gauss sin 0*/
+	/*
 	h.Imprimir();
 	Matriz j = h.Gauss();	
 	h.Imprimir();
 	j.Imprimir();
 	*/
 
-	/* Prueba permutar
+	/* Prueba permutar*/
+	/*
 	h.Imprimir();
 	h.Permutar(1,2);
 	h.Imprimir();
 	*/
+
+	/* Prueba gauss con 0 */
+	
+	h.Imprimir();	
+	Matriz j = h.Gauss0(p);	
+	h.Imprimir();
+	Imprimir(p);
+	j.Imprimir();
+	
 	
 
 	return 0;
