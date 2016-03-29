9b3d7cc997d18b2fbbcc40637456fabca3fc5021
lew axel ariel <alew@ws16.labo7>
2016-03-29 16:35:41 -0300

    generador

---
+++ Gauss/genRand.cpp
@@ -0,0 +1,50 @@
+#include <time.h>
+#include <vector>
+#include <algorithm>
+#include <cassert>
+#include <iostream>
+#include <sstream>
+#include <vector>
+#include <math.h>
+#include <sstream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int main(int argc, char* argv[]){
+
+	
+
+	
+	if (argc != 4 ){
+		printf("%s [Cantidad de equipos] [Cantidad de partidos] [OUTPUT FILE]\n", argv[0]);
+	} else{
+		srand(time(NULL));
+		int cantEquipos = atoi(argv[1]);
+		int cantPartidos = atoi(argv[2]);
+
+
+
+		FILE* out = fopen(argv[3], "w");
+
+		fprintf(out, "%d %d\n", cantEquipos, cantPartidos);
+		
+		
+
+		// PUEDE HABER EQUIPOS SIN JUGAR
+		for (int i = 0; i<cantPartidos; i++){
+			int equip1 = (rand()%cantEquipos)+1;
+			int equip2 = (rand()%cantEquipos)+1;
+			if (equip1 != equip2){
+				fprintf(out, "1 %d 1 %d 0\n", equip1, equip2);
+			} else{
+				i--;
+			}
+		}
+		fclose(out);
+	}
+	
+	return 0;
+  
+}
