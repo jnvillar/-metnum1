f8cd5bc09920078327a4d93aa00480628412a350
lew axel ariel <alew@ws6.labo7>
2016-04-01 20:12:12 -0300

    putos

---
+++ Data/Scripts/Futbol.py
@@ -0,0 +1,61 @@
+import csv
+import sys
+from sets import Set
+
+
+input_path = sys.argv[1]
+output_path = sys.argv[2]
+
+with open(input_path,'rb') as csvfile,open(output_path,'w') as outfile:
+	score_reader = csv.reader(csvfile,delimiter=',',skipinitialspace=True)
+	# Skip the header
+	next(score_reader, None)
+	teams = {}
+	n_games = 0
+	n_teams = 1
+	yaLoHice = False
+	fecha = 1
+	jugadosEstaFecha = 0
+	repiteEquipo = False
+	# String with info of the games.
+	out_str = ''
+	for row in score_reader:
+		#idTeam = 0
+		if row[2] in teams:
+			idTeam = teams[row[2]]
+			repiteEquipo = True
+		else:
+			teams[row[2]] = n_teams
+			idTeam = n_teams
+			n_teams = n_teams + 1
+
+		if row[3] in teams:
+			idTeam2 = teams[row[3]]
+			repiteEquipo = True	
+		else:
+			teams[row[3]] = n_teams
+			idTeam2 = n_teams
+			n_teams = n_teams + 1
+
+		if (repiteEquipo and not yaLoHice):
+			yaLoHice = True
+			cantPartidosPorFecha = (n_teams+1)/2
+			jugadosEstaFecha = 0
+			fecha = fecha+1
+
+		if (yaLoHice):
+			if jugadosEstaFecha == cantPartidosPorFecha:
+				jugadosEstaFecha = 1
+				fecha = fecha+1
+			else:
+				jugadosEstaFecha = jugadosEstaFecha + 1
+
+		
+		if row[4] != row[5]:
+			out_str = out_str + str(fecha) + ' ' + str(idTeam) + ' ' + row[4] + ' ' + str(idTeam2) + ' ' + row[5] + '\n'
+			n_games = n_games+1
+	# Write number of teams and games
+	outfile.write(str(len(teams)) + ' ' + str(n_games) + '\n')
+	# Write games details
+	outfile.write(out_str)
+
---
+++ Gauss/victorias.cpp
@@ -0,0 +1,86 @@
+#include <sstream>
+#include <fstream>
+#include <vector>
+#include <cassert>
+#include <iostream>
+#include <sstream>
+#include <vector>
+#include <sstream>
+#include <algorithm>
+
+
+
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+
+	vector<int> jugados;
+	vector<int> ganados;
+	int equipos;
+	int partidos;
+	if (argc != 3){
+		printf("%s [INPUT FILE] [OUTPUT FILE]\n", argv[0]);
+	} else{
+		ifstream in(argv[2]);
+		if (in.is_open()){
+			string str;
+			getline(in, str);
+			istringstream ist;	
+			ist.str(str);
+			str.clear();
+			ist>>equipos;
+			ist>>partidos;
+			for (int i = 0; i<equipos; i++){
+				jugados.push_back(0);
+				ganados.push_back(0);
+			}
+
+			// Parseo del resto del archivo
+			string st;
+			while (getline(in, st)){
+				istringstream iss;
+				iss.str(st);
+
+				//Aca extraemos la info de la línea en la que estamos
+
+				int fecha;
+				iss>> fecha;
+
+
+				int equipo1;
+				iss>> equipo1;
+				equipo1--;		// Asi el equipo 1 es el 0 
+				int goles1;
+				iss>> goles1;
+				int equipo2;
+				iss>> equipo2;
+				equipo2--;
+				int goles2;
+				iss>> goles2;
+
+				if (goles1 < goles2){
+					ganados[equipo2]++;
+				} else {
+					ganados[equipo1]++;	
+				}
+				jugados[equipo1]++;
+				jugados[equipo2]++;		
+
+			}	
+
+		}
+
+	}
+
+	FILE* out = fopen(argv[3], "w");
+	for(int i = 0; i< equipos; i++){
+		float res = (float) ganados[i]/ (float)jugados[i];
+		fprintf(out, "%f\n", res);
+	}
+	fclose(out);
+	
+	return 0;
+  
+}
