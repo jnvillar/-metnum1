f9df0830acb006eff931887140ab7be1e58fb5e1
juany360 <jnoli@ws5.labo7>
2016-04-01 17:33:59 -0300

    experimento1, graficos, etc

--- Experimentaciones/Experimento 1/pruebas.cpp
+++ Experimentaciones/Experimento 1/pruebas.cpp
@@ -7,60 +7,76 @@
 /* retorna "a - b" en segundos */
 double timeval_diff(struct timeval *a, struct timeval *b)
 {
-  return
-    (double)(a->tv_sec + (double)a->tv_usec/1000000) -
-    (double)(b->tv_sec + (double)b->tv_usec/1000000);
+return
+(double)(a->tv_sec + (double)a->tv_usec/1000000) -
+(double)(b->tv_sec + (double)b->tv_usec/1000000);
 }
 
 int main(int argc, char *argv[]){
-  struct timeval gauss_ini, gauss_fin;
-  struct timeval chol_ini, chol_fin;
-  double secs1;
-  double secs2;
-  
-  FILE*  arch1= fopen("resgauss", "w");
-  FILE*  arch2= fopen("reschol", "w");
-  
+struct timeval gauss_ini, gauss_fin;
+struct timeval chol_ini, chol_fin;
+double secs1;
+double secs2;
+double secs1acum;
+double secs2acum;
 
-  int equipos;
-  cout << "Ingresa cantidad de equipos" << endl;
-  cin >> equipos;
+FILE*  arch1= fopen("resgauss", "w");
+FILE*  arch2= fopen("reschol", "w");
 
-  int enfrentamientos;
-  cout << "Ingresa cantidad de enfrentamientos" << endl;
-  cin >> enfrentamientos;
 
- char* ruta = "resultados";
+int equipos;
+cout << "Ingresa cantidad de equipos" << endl;
+cin >> equipos;
 
-  int i = 0;
+int enfrentamientos;
+cout << "Ingresa cantidad de enfrentamientos" << endl;
+cin >> enfrentamientos; 
 
-  while(i<100){
+int resolver;
+cout << "Ingresa cantidad de enfrentamientos" << endl;
+cin >> resolver; 
 
-  	equipos = equipos + 10;
+char* ruta = "resultados";
+
+int i = 0;
+
+while(i<100){
+
+	equipos = equipos + 10;
 	generador(equipos,enfrentamientos,ruta);
 
 	Matriz g = parser(ruta);
-	Matriz c = parser(ruta);
+	secs1acum = 0;
+	secs2acum = 0;
+	int j = 0;
 
-	
+	while(j<resolver){
+
+	Matriz a = g;	
+	Matriz b = g;
 
 	gettimeofday(&gauss_ini, NULL);
-	g.Gauss0();
-	g.resolverTriangSup();
+	a.Gauss0();
+	a.resolverTriangSup();
 	gettimeofday(&gauss_fin, NULL);
 	secs1 = timeval_diff(&gauss_fin, &gauss_ini);
-	fprintf(arch1, "%f\n", secs1);
-
-
+	secs1acum = secs1acum +secs1;
 
 	gettimeofday(&chol_ini, NULL);
-	c.cholesky();
-	c.resolverTriangSup();
-	c.resolverTriangSupTraspuesta();
+	b.cholesky();
+	b.resolverTriangSup();
+	b.resolverTriangSupTraspuesta();
 	gettimeofday(&chol_fin, NULL);
 	secs2 = timeval_diff(&chol_fin, &chol_ini);
-	fprintf(arch2, "%f\n", secs2);
-	i++;
+	secs2acum = secs2acum + secs2;
+	j++;
+	}
+
+fprintf(arch1, "%f\n", secs1acum/resolver);
+fprintf(arch2, "%f\n", secs2acum/resolver);
+
+
+i++;
 }
 
 fclose(arch1);
@@ -68,5 +84,5 @@ fclose(arch2);
 
 
 
-  return 0;
+return 0;
 }
