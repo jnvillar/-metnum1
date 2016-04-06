5b366ce41b02ab82497a4df56ff06bf32d889af0
juany360 <jnoli@ws10.labo1>
2016-04-06 17:34:14 -0300

    holis

--- Experimentaciones/Estrategia para llegar al primer lugar/main.cpp
+++ Experimentaciones/Estrategia para llegar al primer lugar/main.cpp
@@ -2,14 +2,14 @@
 
 int main(int argc, char* argv[]){	
 	int equipo;
-	cout << "Elija el equipo" << endl;
+	cout << "Elija el equipo, note que el equipo 0 es el 1" << endl;
 	cin >> equipo;		
 	Matriz m(parser(argv[1],equipo-1));		
 	Matriz original(parser(argv[1]));
 	original.cholesky();
 	original.resolverTriangInf();
 	original.resolverTriangSupTraspuesta();	
-	cout << "Rankings Originales" << endl << endl;
+	cout << endl <<  "Rankings Originales" << endl << endl;
 	original.ImprimirSolucionOrdeneda(stdout);	
 	m.estrategia1(equipo-1);		
 	return 0;  
